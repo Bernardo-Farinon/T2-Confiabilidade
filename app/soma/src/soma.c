355b52407fbdf8351ab7115c9a0a2e16303f89a8
VLSI1 ECo <vlsi1_4@paxos.inf.pucrs.br>
2025-11-11 20:19:06 -0300

    add app soma

---
+++ app/soma/src/soma.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+
+int main() {
+
+    printf("Iniciando teste...\n");
+
+    int a = 10;
+    int b = 20;
+    int c = a + b;
+
+    printf("Resultado: %d\n", c);
+
+    return 0;
+}
